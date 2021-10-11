aba85d68e19293b4e11ddce2c0ff446705417c33
Azamat <sab288@yandex.ru>
2021-10-11 14:03:28 +0600

    функции добавлен возвращаемый тип, фигурная скобка перенесена на новую строку согласно PSR-2: coding style guide

--- index.php
+++ index.php
@@ -42,7 +42,8 @@ $tasks =
         ]
     ];
 
-    function countTasks(array $tasks, string $projectName) {
+    function countTasks(array $tasks, string $projectName) : int
+    {
         $result = 0;
             foreach ($tasks as $task) {
                 if ($task['category'] === $projectName) {
