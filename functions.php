b3bc7f9d665a14e5fbcad760d9d6a2efc21a0d9b
Azamat <sab288@yandex.ru>
2021-10-15 11:01:37 +0600

    созданы шаблоны main и layout в папке templates, включена безопасность от XSS атак, в index подключены данные и функции для передачи в шаблонизатор

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+function countTasks(array $tasks, string $projectName): int
+{
+    $result = 0;
+    foreach ($tasks as $task) {
+        if ($task['category'] === $projectName)
+        {
+            $result++;
+        }
+    }
+    return $result;
+}
