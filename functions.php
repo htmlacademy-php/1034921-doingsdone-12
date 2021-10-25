f8d5c11627200fde4f5b529fe374d0e4ff5dd208
Azamat <sab288@yandex.ru>
2021-10-25 11:25:19 +0600

    логика функции checkHours изменена на пересчет timestamp, добавлена константа hourEpoch для расчета часов

--- functions.php
+++ functions.php
@@ -12,16 +12,14 @@ function countTasks(array $tasks, string $projectName): int
 }
 
 # функция возвращает истину если разница текущего времени и датой задания
-# меньше аргумента hours, по условию задания аргумент hours = 24 часа
+# меньше аргумента hours, по условию задания параметр hours = 24 часа
 # переменная hoursBeforeTask = 24 в index передает аргумент функции в main
 
 function checkHours(int $hours, string $date): bool
 {
-    $currentDate = date_create('now');
-    $nextDate = date_create($date);
-    $diff = date_diff($currentDate, $nextDate);
-    $diffHours = date_interval_format($diff, '%h');
-    $diffDays = date_interval_format($diff, '%d');
-    #echo '<pre>', print_r($diff), '</pre>';
-    return ($diffHours <= $hours) && ($diffDays === '0');
+    $now = time();
+    $taskDate = strtotime($date);
+    $diff = floor($taskDate - $now);
+    return ($diff > 0) && (($diff / hourEpoch) <= $hours);
 }
+
