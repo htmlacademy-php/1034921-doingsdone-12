0b7d1b313844521fa54aaed496539e727a00bd55
Azamat <sab288@yandex.ru>
2021-11-15 12:21:31 +0600

    добавлены функции getTasksByUser, getProjectsByUser с параметром id пользователя для отображения проектов и задач пользователя

--- functions.php
+++ functions.php
@@ -23,3 +23,26 @@ function checkHours(int $hours, string $date): bool
     return ($diff > 0) && (($diff / HOUR_SECONDS) <= $hours);
 }
 
+function getProjectsByUser(object $connect, int $userId): array
+{
+    $result = [];
+    $query = "SELECT name FROM project WHERE user_id = ?";
+    $stmt = mysqli_prepare($connect, $query);
+    mysqli_stmt_bind_param($stmt, 'i', $userId);
+    mysqli_stmt_execute($stmt);
+    $resultSql = mysqli_stmt_get_result($stmt);
+    $result = mysqli_fetch_all($resultSql, MYSQLI_ASSOC);
+    return $result;
+}
+
+function getTasksByUser(object $connect, int $userId): array
+{
+    $result = [];
+    $query = "SELECT t.name, t.state AS isDone, t.expiration AS date, p.name AS category FROM task as t INNER JOIN project as p ON p.user_id = ? INNER JOIN user as u ON u.id = ? WHERE t.project_id = p.id";
+    $stmt = mysqli_prepare($connect, $query);
+    mysqli_stmt_bind_param($stmt, 'ii', $userId, $userId);
+    mysqli_stmt_execute($stmt);
+    $resultSql = mysqli_stmt_get_result($stmt);
+    $result = mysqli_fetch_all($resultSql, MYSQLI_ASSOC);
+    return $result;
+}
--- index.php
+++ index.php
@@ -3,31 +3,16 @@
 require 'data.php';
 require 'functions.php';
 require 'helpers.php';
-require 'db.php';
 
 $connect = mysqli_connect('localhost', 'root', '', 'doingsdone');
 mysqli_set_charset($connect, 'utf8');
-
-if (!$connect) {
-    print_r('error: ', mysqli_connect_error());
-}
+  
 $userId = 1;
-$sqlUserProjects = "SELECT name FROM project WHERE user_id = ?";
-$sqlUserTasks = "SELECT t.name, t.state AS isDone, t.expiration AS date, p.name AS category FROM task as t JOIN project as p ON p.user_id = ? JOIN user as u ON u.id = ? WHERE t.project_id = p.id";
 
-$stmt = mysqli_prepare($connect, $sqlUserProjects);
-mysqli_stmt_bind_param($stmt, 'i', $userId);
-mysqli_stmt_execute($stmt);
-$resultSqlUserProjects = mysqli_stmt_get_result($stmt);
-$projects = mysqli_fetch_all($resultSqlUserProjects, MYSQLI_ASSOC);
+$projects = getProjectsByUser($connect, $userId);
+$tasks = getTasksByUser($connect, $userId);
 
-$stmt = mysqli_prepare($connect, $sqlUserTasks);
-mysqli_stmt_bind_param($stmt, 'ii', $userId, $userId);
-mysqli_stmt_execute($stmt);
-$resultSqlUserTasks = mysqli_stmt_get_result($stmt);
-$tasks = mysqli_fetch_all($resultSqlUserTasks, MYSQLI_ASSOC);
-
-mysqli_close($connect);
+mysqli_close($connect);    
 
 $content = include_template('main.php',
     [
